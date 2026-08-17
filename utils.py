a3cfa24d579bc5b9609d35e368739f8c127108f0
JulioBap <schmidtjulio11@gmail.com>
2026-08-17 13:08:47 -0300

    css implementado

--- utils.py
+++ utils.py
@@ -1,21 +1,21 @@
 import json
 def load_data(arquivo_json):
     caminho = f"static/data/{arquivo_json}"
-    with open(caminho) as arquivo:
+    with open(caminho, encoding='utf-8') as arquivo:
         dados = json.load(arquivo)
         return dados
 
 
 def load_template(arquivo_template):
     caminho = f"static/templates/{arquivo_template}"
-    with open(caminho) as arquivo:
+    with open(caminho, encoding='utf-8') as arquivo:
         dados = arquivo.read()
         return dados
 
 def recebe_anotacao(anotacao):
     caminho = 'static/data/notes.json'
-    with open(caminho) as arquivo:
+    with open(caminho, encoding='utf-8') as arquivo:
         dados = json.load(arquivo)
         dados.append(anotacao)
-    with open(caminho, 'w') as arquivo:
-        json.dump(dados,arquivo)
+    with open(caminho, 'w', encoding='utf-8') as arquivo:
+        json.dump(dados, arquivo, ensure_ascii=False, indent=4)
